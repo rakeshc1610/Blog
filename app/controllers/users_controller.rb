8875b106c286212417e0fc78910184327384fb44
rakeshc1610 <rakeshc.d*6@gmail.com>
2016-05-03 19:40:02 -0400

    third commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	def index
-	  @users=User.all
+	  @users=User.order("created_at DESC").limit(10)
 	end
 	def new
 		@user=User.new
@@ -10,17 +10,29 @@ class UsersController < ApplicationController
 		@user=User.new(user_params)
 	  if @user.save
 	  	flash[:notice] = "Record Saved Succesfullly"
-	    redirect_to action: "index"
-	  else
+	    redirect_to action: "show", id: @user.id
+  else
 	   flash[:notice] = "Error in saving Record"
 	   render :new
 	  end
 	end
 	def edit
+		@user = User.find_by(id: params[:id])
 	end
 	def update
+	  @user=User.find_by(id: params[:id])
+		if @user.update_attributes(user_params)
+			flash[:notice] = "Record updated successfully"
+			redirect_to action: "show", id: @user.id
+		else
+			flash[:notice] = "Error in updating the record"
+			render :edit
+		end
 	end
+
 	def show
+
+		@user = User.find_by(id: params[:id])
 	end
 
 	def destroy
